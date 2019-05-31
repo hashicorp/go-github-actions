9463dfc497cc4217e0ed63acf10d2e6d0ccfaab4
findkim <kngo@hashicorp.com>
2019-05-31 11:26:06 -0500

    vendor-diff: support push events and comment on open PRs

--- vendor-diff/entrypoint.sh
+++ vendor-diff/entrypoint.sh
@@ -15,12 +15,9 @@ if [[ $SUCCESS -eq 0 ]]; then
 	exit 0
 fi
 
-if [[ "$GITHUB_EVENT_NAME" != "pull_request" ]]; then
-	exit $SUCCESS
-fi
-
-# Add comment on PR with vendor diff
-COMMENT="#### Vendor directory is not in sync with \`go.mod\`
+payload () {
+	# Generate comment payload for PR with vendor diff
+	COMMENT="#### Vendor directory is not in sync with \`go.mod\`
 \`\`\`
 $DIFF
 \`\`\`
@@ -29,10 +26,33 @@ Run \`go mod vendor\` to update the vendor directory and commit the changes.
 
 *Workflow: \`$GITHUB_WORKFLOW\`, Action: \`$GITHUB_ACTION\`*"
 
-PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
-COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
-curl -s -S -H "Authorization: token $GITHUB_TOKEN" \
-	--header "Content-Type: application/json" \
-	--data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
+	echo '{}' | jq --arg body "$COMMENT" '.body = $body'
+}
+
+action=$(cat /github/workflow/event.json | jq -r .action)
+
+if [[ "$GITHUB_EVENT_NAME" == "pull_request" && "$action" == "opened" ]]; then
+	# Comment only when PRs are first opened
+	comment=$(payload)
+	url=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
+	curl -s -S -H "Authorization: token $GITHUB_TOKEN" \
+		--header "Content-Type: application/json" \
+		--data "$comment" "$url"
+
+elif [[ "$GITHUB_EVENT_NAME" == "push" ]]; then
+	# Fetch PRs associated with commit from push event to post diff comments
+	comment=$(payload)
+	pr_url="https://api.github.com/repos/${GITHUB_REPOSITORY}/commits/${GITHUB_SHA}/pulls"
+	urls=$(curl -s -S -H "Authorization: token $GITHUB_TOKEN" \
+		--header "Content-Type: application/json" \
+		--header "Accept: application/vnd.github.groot-preview+json" \
+		"$pr_url" | jq -r '.[] | select(.state=="open") | .comments_url')
+
+	for url in $urls; do
+		curl -s -S -H "Authorization: token $GITHUB_TOKEN" \
+		--header "Content-Type: application/json" \
+		--data "$comment" "$url"
+	done
+fi
 
 exit $SUCCESS
