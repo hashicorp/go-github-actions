96fefebe2a324e3256d400838faf593c36a5c119
findkim <kngo@hashicorp.com>
2019-06-03 12:39:22 -0500

    vendor-diff: rm commenting on pull_request events and defer to push events

--- vendor-diff/entrypoint.sh
+++ vendor-diff/entrypoint.sh
@@ -29,17 +29,7 @@ Run \`go mod vendor\` to update the vendor directory and commit the changes.
 	echo '{}' | jq --arg body "$COMMENT" '.body = $body'
 }
 
-action=$(cat /github/workflow/event.json | jq -r .action)
-
-if [[ "$GITHUB_EVENT_NAME" == "pull_request" && "$action" == "opened" ]]; then
-	# Comment only when PRs are first opened
-	comment=$(payload)
-	url=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
-	curl -s -S -H "Authorization: token $GITHUB_TOKEN" \
-		--header "Content-Type: application/json" \
-		--data "$comment" "$url"
-
-elif [[ "$GITHUB_EVENT_NAME" == "push" ]]; then
+if [[ "$GITHUB_EVENT_NAME" == "push" ]]; then
 	# Fetch PRs associated with commit from push event to post diff comments
 	comment=$(payload)
 	pr_url="https://api.github.com/repos/${GITHUB_REPOSITORY}/commits/${GITHUB_SHA}/pulls"
